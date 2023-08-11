3520342dde6a37b80d212269219cbcd9a891416f
isasmelo <isabelamelos15@gmail.com>
2023-08-11 21:59:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // if(html.classList.contains("light")){
+  //   html.classList.remove("light")
+  // } else{
+  //   html.classList.add("light")
+  // }
+
+  // trocar imagem//
+  // const img = document.querySelector("#profile img")
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src","./linkimage-ligth")
+  // } else {
+  //   img.setAttribute("src", "./linkimage-dark")
+  // }
+  //troca descriçao//
+//   const img = document.querySelector("#profile img")
+//   if (html.classList.contains("light")) {
+//     img.setAttribute("alt","logo do meu pau")
+//   } else {
+//     img.setAttribute("alt", "logo do jupiter atelier de tatuagem")
+// }
+}
